b86a2e8671277dbb436311d41e53be85e6934beb
boldask <boldaskshow@gmail.com>
2025-12-30 13:33:35 -0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,24 @@ import 'providers/app_state_provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    // TODO: Add your Firebase options here
-    // options: DefaultFirebaseOptions.currentPlatform,
-  );
+  if (kIsWeb) {
+    // THIS IS THE FIX: Explicitly pass your web credentials here
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        aapiKey: "AIzaSyBWrmfI_fa0wo4uHm2HqUO_geimmPGsP6s",
+        authDomain: "boldask-150.firebaseapp.com",
+        databaseURL: "https://boldask-150-default-rtdb.firebaseio.com",
+        projectId: "boldask-150",
+        storageBucket: "boldask-150.firebasestorage.app",
+        messagingSenderId: "772421956417",
+        appId: "1:772421956417:web:de5808c2f6193cd1b6a7c0",
+        measurementId: "G-L3HQ7QZR26"
+      ),
+    );
+  } else {
+    // For Android/iOS, it reads from google-services.json or GoogleService-Info.plist
+    await Firebase.initializeApp();
+  }
 
   runApp(
     MultiProvider(
